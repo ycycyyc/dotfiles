38d5d9af579e617f9da628084968d1fdd309c18c
ycycyyc <944696498@qq.com>
2023-04-04 14:39:38 +0800

    nvim: map nvim-tree keys

--- nvim/.config/nvim/lua/plug_conf/tree.lua
+++ nvim/.config/nvim/lua/plug_conf/tree.lua
@@ -15,6 +15,24 @@ M.config = function()
       },
     },
     filters = { dotfiles = false },
+
+    on_attach = function(bufnr)
+      local api = require "nvim-tree.api"
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+      vim.keymap.set("n", "[c", api.node.navigate.git.prev, opts "Prev Git")
+      vim.keymap.set("n", "]c", api.node.navigate.git.next, opts "Next Git")
+      vim.keymap.set("n", "<CR>", api.node.open.edit, opts "Open")
+      vim.keymap.set("n", "o", api.node.open.edit, opts "Open")
+      vim.keymap.set("n", "a", api.fs.create, opts "Create")
+      vim.keymap.set("n", "<Tab>", api.node.open.preview, opts "Open Preview")
+      vim.keymap.set("n", "r", api.fs.rename, opts "Rename")
+      vim.keymap.set("n", "R", api.tree.reload, opts "Refresh")
+      vim.keymap.set("n", "y", api.fs.copy.filename, opts "Copy Name")
+      vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts "Open: Vertical Split")
+      vim.keymap.set("n", "d", api.fs.remove, opts "Delete")
+    end,
   }
   --
   vim.cmd [[highlight NvimTreeSymlink guifg=blue gui=bold,underline]]
