4635937b0e67ce1ea4e7b2a4fe93c4b3ffeafef6
ycycyyc <944696498@qq.com>
2023-04-09 18:42:10 +0800

    nvim: remove lsp kind

--- nvim/.config/nvim/lua/yc/lazy_plug.lua
+++ nvim/.config/nvim/lua/yc/lazy_plug.lua
@@ -148,11 +148,9 @@ local plugins = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
+
       "hrsh7th/cmp-vsnip",
       "hrsh7th/vim-vsnip",
-      "onsails/lspkind-nvim",
     },
   },
 
