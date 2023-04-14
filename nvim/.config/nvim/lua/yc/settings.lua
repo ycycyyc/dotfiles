8dfc8180f6c011640d433e0fc7d7c828361dd663
ycycyyc <944696498@qq.com>
2023-04-14 17:49:11 +0800

    nvim: add after dir

--- nvim/.config/nvim/lua/yc/settings.lua
+++ nvim/.config/nvim/lua/yc/settings.lua
@@ -53,13 +53,6 @@ M.setup = function()
 
   local helper = require "utils.helper"
 
-  M.register_fts_cb("lua", function()
-    vim.opt.tabstop = 2
-    vim.opt.softtabstop = 2
-    vim.opt.sw = 2
-    vim.opt.expandtab = true -- 在插入模式下，会把按 Tab 键所插入的 tab 字符替换为合适数目的空格。如果确实要插入 tab 字符，需要按 CTRL-V 键，再按 Tab 键
-  end)
-
   M.register_fts_cb("qf", function()
     vim.cmd [[wincmd J]]
   end)
