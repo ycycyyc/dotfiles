19885ad828d5efde78a081f2719e6b6a84b76b21
ycycyyc <944696498@qq.com>
2023-08-06 17:13:01 +0800

    map(nvim-tree): map q to exit

--- nvim/.config/nvim/lua/plug_conf/tree.lua
+++ nvim/.config/nvim/lua/plug_conf/tree.lua
@@ -35,6 +35,7 @@ M.config = function()
       vim.keymap.set("n", "d", api.fs.remove, opts "Delete")
       vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts "Toggle Dotfiles")
       vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts "Toggle Git Ignore")
+      vim.keymap.set("n", "q", api.tree.close, opts "Close the tree")
     end,
   }
   --
