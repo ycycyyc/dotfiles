b735392b6fbbd1681b7690d8e11bc0b475a6e474
ycycyyc <944696498@qq.com>
2023-08-27 18:56:48 +0800

    nvim: remove pairs match plugin

--- nvim/.config/nvim/lua/yc/lazy_plug.lua
+++ nvim/.config/nvim/lua/yc/lazy_plug.lua
@@ -15,22 +15,6 @@ local basic_plugins = {
   "nvim-lua/plenary.nvim",
 
   {
-    "utilyre/sentiment.nvim",
-    event = "VeryLazy", -- keep for lazy loading
-    opts = {
-      limit = 55,
-      included_modes = {
-        n = true,
-        i = false,
-      },
-    },
-    init = function()
-      -- `matchparen.vim` needs to be disabled manually in case of lazy loading
-      vim.g.loaded_matchparen = 1
-    end,
-  },
-
-  {
     "ycycyyc/flash.nvim",
     branch = "main",
     event = "VeryLazy",
