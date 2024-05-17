1f6edc6e17595086b180f49345a673e729090ce0
ycycyyc <944696498@qq.com>
2024-05-17 23:21:10 +0800

    [feat]nvim: lspProgress for neovim 0.10

--- nvim/.config/nvim/lua/yc/lsp/progress.lua
+++ nvim/.config/nvim/lua/yc/lsp/progress.lua
@@ -27,37 +27,40 @@ M.update = function()
   show:show(content)
 end
 
+-- neovim 0.10
+M.updateV2 = function(status)
+  if status and status.data and status.data.params and status.data.params.value then
+    local client_id = status.data.client_id
+    local name = ""
+    if client_id then
+      name = vim.lsp.get_client_by_id(client_id).name or ""
+    end
+
+    local res = status.data.params
+    local kind = res.value.kind or "end"
+    local msg = res.value.message or ""
+    local title = res.value.title or ""
+    local percentage = res.value.percentage or 0
+
+    if kind == "end" then
+      vim.defer_fn(function()
+        show:show ""
+      end, 1000)
+    else
+      local info = string.format("%s: %s %s (%s%%) ", name, title, msg, percentage)
+      show:show(info)
+    end
+  else
+    vim.defer_fn(function()
+      show:show ""
+    end, 1000)
+  end
+end
+
 M.setup = function()
   if vim.fn.has "nvim-0.10" == 1 then
     vim.api.nvim_create_autocmd({ "LspProgress" }, {
-      callback = function(status)
-        if status and status.data and status.data.result and status.data.result.value then
-          local client_id = status.data.client_id
-          local name = ""
-          if client_id then
-            name = vim.lsp.get_client_by_id(client_id).name or ""
-          end
-
-          local res = status.data.result
-          local kind = res.value.kind or "end"
-          local msg = res.value.message or ""
-          local title = res.value.title or ""
-          local percentage = res.value.percentage or 0
-
-          if kind == "end" then
-            vim.defer_fn(function()
-              show:show ""
-            end, 1000)
-          else
-            local info = string.format("%s: %s %s (%s%%) ", name, title, msg, percentage)
-            show:show(info)
-          end
-        else
-          vim.defer_fn(function()
-            show:show ""
-          end, 1000)
-        end
-      end,
+      callback = M.updateV2,
     })
   else
     vim.api.nvim_create_autocmd({ "User" }, {
