3f02794b20d9a3e4ac756458941e1b469d227419
ycycyyc <944696498@qq.com>
2024-05-20 11:17:08 +0800

    'gc' can load Comment.nvim

--- nvim/.config/nvim/lua/yc/lazy_plug.lua
+++ nvim/.config/nvim/lua/yc/lazy_plug.lua
@@ -93,12 +93,12 @@ local basic_plugins = {
   {
     "numToStr/Comment.nvim",
     keys = {
-      { "gc", mode = { "x" } },
+      { "gc", mode = { "x", "n" } },
       { "gcc", mode = { "n" } },
     },
     opts = {},
     -- 0.10版本自动注释功能不好用
-    -- cond = vim.fn.has "nvim-0.10" == 0, 
+    -- cond = vim.fn.has "nvim-0.10" == 0,
   },
 
   {
