34e0aab2540a10256781d0dc779f5f9d8661c842
ycycyyc <944696498@qq.com>
2024-05-24 22:29:08 +0800

    don't have `o` add a comment for lua file

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -2,3 +2,5 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.sw = 2
 vim.opt_local.expandtab = true -- 在插入模式下，会把按 Tab 键所插入的 tab 字符替换为合适数目的空格。如果确实要插入 tab 字符，需要按 CTRL-V 键，再按 Tab 键
+-- Don't have `o` add a comment
+vim.opt.formatoptions:remove "o"
