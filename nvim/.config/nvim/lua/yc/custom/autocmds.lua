ed421fb3b02dcbeaf6b8dd02be6ef3e38e888b15
ycycyyc <944696498@qq.com>
2025-06-04 09:54:36 +0800

    feat: vim.highlight => vim.hl

--- nvim/.config/nvim/lua/yc/custom/autocmds.lua
+++ nvim/.config/nvim/lua/yc/custom/autocmds.lua
@@ -4,7 +4,7 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
   callback = function()
-    vim.highlight.on_yank { timeout = 300 }
+    vim.hl.on_yank { timeout = 300 }
   end,
 })
 
