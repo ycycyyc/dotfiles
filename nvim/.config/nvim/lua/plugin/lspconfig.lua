3953c65ae61a7e846500d98194720663220a1f34
ycycyyc <944696498@qq.com>
2025-09-22 17:21:34 +0800

    feat: use vim.lsp.config instead of require"lspconfig".xxx.setup

--- nvim/.config/nvim/lua/plugin/lspconfig.lua
+++ nvim/.config/nvim/lua/plugin/lspconfig.lua
@@ -1,44 +1,23 @@
 -- install lsp-server from
 -- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
-local env = YcVim.env
-
 return {
   "neovim/nvim-lspconfig",
   event = "User FilePost",
   config = function()
-    local lspconfig = require "lspconfig"
-    local configs = require "lspconfig.configs"
-
-    if not configs.emmylua_ls then
-      local root_files = {
-        ".luarc.json",
-        ".luarc.jsonc",
-        ".emmyrc.json",
-        ".git",
-      }
-
-      local util = require "lspconfig.util"
-
-      configs.emmylua_ls = {
-        default_config = {
-          cmd = { "emmylua_ls" },
-          filetypes = { "lua" },
-          root_dir = util.root_pattern(root_files),
-        },
-      }
-    else
-      vim.notify "emmylua_ls is added in nvim-lspconfig"
-    end
-
+    local env = YcVim.env
     for name, opt in pairs(YcVim.lsp.servers) do
-      lspconfig[name].setup(vim.tbl_deep_extend("error", opt, {
-        on_init = function(client, _)
-          if client.server_capabilities and not env.semantic_token then
-            client.server_capabilities.semanticTokensProvider = false
-          end
-        end,
-        capabilities = YcVim.lsp.capabilities(name),
-      }))
+      vim.lsp.config(
+        name,
+        vim.tbl_deep_extend("error", opt, {
+          on_init = function(client, _)
+            if client.server_capabilities and not env.semantic_token then
+              client.server_capabilities.semanticTokensProvider = false
+            end
+          end,
+          capabilities = YcVim.lsp.capabilities(name),
+        })
+      )
+      vim.lsp.enable(name)
     end
   end,
 }
