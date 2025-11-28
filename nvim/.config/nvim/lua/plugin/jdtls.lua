6b697ec688ccc6ea1b8a9aa2ff0a6487c423b5b9
ycycyyc <944696498@qq.com>
2025-11-28 16:25:20 +0800

    feat: add nvim-jdtls plugin

---
+++ nvim/.config/nvim/lua/plugin/jdtls.lua
@@ -0,0 +1,101 @@
+local java_filetypes = { "java" }
+
+local function extend_or_override(config, custom, ...)
+  if type(custom) == "function" then
+    config = custom(config, ...) or config
+  elseif custom then
+    config = vim.tbl_deep_extend("force", config, custom) --[[@as table]]
+  end
+  return config
+end
+
+return {
+  "mfussenegger/nvim-jdtls",
+  ft = java_filetypes,
+  cond = 1 == vim.fn.executable "jdtls",
+  opts = function()
+    local cmd = { vim.fn.exepath "jdtls" }
+    return {
+      root_dir = function(path)
+        return vim.fs.root(path, ".classpath")
+      end,
+
+      -- How to find the project name for a given root dir.
+      project_name = function(root_dir)
+        return root_dir and vim.fs.basename(root_dir)
+      end,
+
+      -- Where are the config and workspace dirs for a project?
+      jdtls_config_dir = function(project_name)
+        return vim.fn.stdpath "cache" .. "/jdtls/" .. project_name .. "/config"
+      end,
+      jdtls_workspace_dir = function(project_name)
+        return vim.fn.stdpath "cache" .. "/jdtls/" .. project_name .. "/workspace"
+      end,
+
+      -- How to run jdtls. This can be overridden to a full java command-line
+      -- if the Python wrapper script doesn't suffice.
+      cmd = cmd,
+      full_cmd = function(opts)
+        local fname = vim.api.nvim_buf_get_name(0)
+        local root_dir = opts.root_dir(fname)
+        local project_name = opts.project_name(root_dir)
+        local cmd2 = vim.deepcopy(opts.cmd)
+        if project_name then
+          vim.list_extend(cmd2, {
+            "-configuration",
+            opts.jdtls_config_dir(project_name),
+            "-data",
+            opts.jdtls_workspace_dir(project_name),
+          })
+        end
+        return cmd
+      end,
+
+      -- These depend on nvim-dap, but can additionally be disabled by setting false here.
+      dap = { hotcodereplace = "auto", config_overrides = {} },
+      -- Can set this to false to disable main class scan, which is a performance killer for large project
+      dap_main = {},
+      test = true,
+      settings = {
+        java = {
+          inlayHints = {
+            parameterNames = {
+              enabled = "all",
+            },
+          },
+        },
+      },
+    }
+  end,
+  config = function(_, opts)
+    local bundles = {} ---@type string[]
+    local function attach_jdtls()
+      local fname = vim.api.nvim_buf_get_name(0)
+
+      -- Configuration can be augmented and overridden by opts.jdtls
+      local config = extend_or_override({
+        cmd = opts.full_cmd(opts),
+        root_dir = opts.root_dir(fname),
+        init_options = {
+          bundles = bundles,
+        },
+        settings = opts.settings,
+        capabilities = YcVim.lsp.capabilities "jdtls",
+      }, opts.jdtls)
+
+      require("jdtls").start_or_attach(config)
+    end
+
+    -- Attach the jdtls for each java buffer. HOWEVER, this plugin loads
+    -- depending on filetype, so this autocmd doesn't run for the first file.
+    -- For that, we call directly below.
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = java_filetypes,
+      callback = attach_jdtls,
+    })
+
+    -- Avoid race condition by calling attach the first time, since the autocmd won't fire.
+    attach_jdtls()
+  end,
+}
