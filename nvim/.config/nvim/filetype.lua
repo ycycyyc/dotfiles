11597a4a0bdea946879a3b8f192e05d57f42f961
ycycyyc <944696498@qq.com>
2025-12-01 14:12:24 +0800

    feat: change the filetype of `.zshrc` to `bash`

--- nvim/.config/nvim/filetype.lua
+++ nvim/.config/nvim/filetype.lua
@@ -1,6 +1,9 @@
 vim.filetype.add {
+  filename = {
+    [".zshrc"] = "bash",
+  },
   pattern = {
-    [".*%.i"] = function(path, bufnr)
+    [".*%.i"] = function(path, _)
       local ok = string.find(path, "third_party/wiredtiger/")
       if ok then
         return "c"
