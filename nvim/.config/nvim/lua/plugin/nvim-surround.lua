75dbc1979ba1d20dcf1cb10a03643c25f4130f3e
ycycyyc <944696498@qq.com>
2026-07-27 19:53:13 +0800

    feat: reconfig `nvim-surround` plugin

--- nvim/.config/nvim/lua/plugin/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugin/nvim-surround.lua
@@ -9,19 +9,6 @@ plugin.keys = {
   { "S", mode = "x" },
 }
 
-plugin.opts = {
-  keymaps = {
-    -- disable this keymap
-    normal_cur = false,
-    normal_line = false,
-    normal_cur_line = false,
-
-    insert = false,
-    insert_line = false,
-
-    visual_line = false,
-    change_line = false,
-  },
-}
+plugin.opts = {}
 
 return plugin
